34cc20aaaa8a214c279bd5fdd31e86efdb2fac0c
jeffersonLouis <jefferson.saleh@gmail.com>
2023-06-26 18:24:16 -0300

    Implemented addition and subtraction to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,23 @@
-public class Calculator {}
+public class Calculator {
+    public static void main(String[] args){}
+
+    public int addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double addition(double a, double b)
+    {
+        return a + b;
+    }
+
+    public int subtraction(int a, int b)
+    {
+        return a - b;
+    }
+
+    public double subtraction(double a, double b)
+    {
+        return a - b;
+    }
+}
