8c67811a19aa92dfb037fa17327ab91c11c45f1c
jeffersonlouis <jeffersonlouissh@gmail.com>
2023-06-26 20:39:29 -0300

    Implemented Division to Calculator.java Class

--- Calculator.java
+++ Calculator.java
@@ -20,4 +20,14 @@ public class Calculator {
     {
         return a - b;
     }
+
+    public int divide(int a, int b)
+    {
+        return a / b;
+    }
+
+    public double divide(double a, double b)
+    {
+        return a / b;
+    }
 }
