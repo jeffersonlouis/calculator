4fc32b7541f91516df3905b69f202c37731bc367
jefferson <jefferson.saleh@gmail.com>
2024-01-25 21:03:44 -0300

    Update calculator.java bug

--- Calculator.java
+++ Calculator.java
@@ -1,4 +1,13 @@
 public class Calculator{
+    public int addition(int a, int b)
+    {
+        return a + b;
+    }
+
+    public double addition(int a, int b)
+    {
+        return b + a;
+    }
 
     public int subtraction(int a, int b)
     {
@@ -8,10 +17,5 @@ public class Calculator{
     public double subtraction(int a, int b)
     {
         return a - b;
-    }
-
-    public times(int a, int b)
-    {
-        return a * b;
     }
 }
